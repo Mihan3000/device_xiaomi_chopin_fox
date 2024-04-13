0ec253ab6d13af8ccc071e1a5972d9c7c8b8cffa
Mihan3000 <121653576+Mihan3000@users.noreply.github.com>
2024-04-13 18:06:50 +0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ruby-user
-add_lunch_combo omni_ruby-userdebug
-add_lunch_combo omni_ruby-eng
+add_lunch_combo omni_chopin-user
+add_lunch_combo omni_chopin-userdebug
+add_lunch_combo omni_chopin-eng
